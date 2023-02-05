6a09a167189a748264aeba928b70c09bfae6b1f2
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-05 17:13:09 +0100

    added the basic structure of elements and components

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -4,7 +4,42 @@ const TodoApp = () => {
   return (
     <div className='mainContainer'>
         <div>
-
+          <input type="text" />
+          <div>
+            <input type="date" name="" id="" />
+            <button>ADD</button>
+          </div>
+        </div>
+        <div>
+          <div>Filter by:</div>
+          <div>
+            <input type="date" name="" id="" />
+            <select name="" id="">
+              <option value="active">Active</option>
+              <option value="completed">Completed</option>
+            </select>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>Tackname</div>
+            <button>Asc</button>
+            <button>Desc</button>
+          </div>
+          <div>
+            <div>Date</div>
+            <button>Asc</button>
+            <button>Desc</button>
+          </div>
+          <div>
+            <div>Status</div>
+            <button>Asc</button>
+            <button>Desc</button>
+          </div>
+        </div>
+        <div>
+          {/* Mapping the task
+          todo component */}
         </div>
     </div>
   )
