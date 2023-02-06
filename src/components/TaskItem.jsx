04941a245e4a59e76bdf89f290a899840cce3cdc
Kiril-Pavlov <Kire_P@yahoo.com>
2023-02-06 19:47:38 +0100

    TaskItem basic contentt added

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const TaskItem = ({tasks,setTasks}) => {
+const TaskItem = ({ tasks, setTasks }) => {
   return (
     <div>
-        {tasks.map(taskItem=>(
-          <div key={taskItem.taskName}>
-            {taskItem.taskName}
+      {tasks.map((taskItem) => (
+        <div key={taskItem.taskName}>
+          <div>
+            <div>{taskItem.status}</div>
+            <div>{taskItem.taskName}</div>
           </div>
-          ))}
+          <div>
+            <div>{taskItem.date}</div>
+            <div>{taskItem.status}</div>
+          </div>
+          <div>
+            controls
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TaskItem
+export default TaskItem;
